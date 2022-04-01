dc84126caead51b866a19e22393165c443fec8b8
Nigel <nigel.davila@agswebconsulting.com>
2022-04-01 13:26:14 -0500

    added plugin to complete tasks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,9 @@ const webpack = require("webpack")
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
+    Popper: ['popper.js', 'default'],
+    JQueryValidation: 'jquery-validation'
+
 }))
 
 module.exports = environment
